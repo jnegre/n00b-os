b65bf05e9b12cc86294bc44b9ba3edd675c8bd24
jnegre <jerome@jnegre.org>
2017-01-15 19:37:05 +0100

    load at 3G

--- kernel/src/arch/i386/tty.c
+++ kernel/src/arch/i386/tty.c
@@ -49,7 +49,7 @@ void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_buffer = (uint16_t*) (0xB8000 + 0xC0000000); //FIXME use a constant
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
