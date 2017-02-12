50614dab69e9b9bfa61328003d6174ed06de4f8b
jnegre <jerome@jnegre.org>
2017-02-12 20:56:37 +0100

    minor edits

--- kernel/include/kernel/panic.h
+++ kernel/include/kernel/panic.h
@@ -1,3 +1,7 @@
 /* Prints a message and stops the computer.
  */
 void panic(const char*) __attribute__((noreturn));
+
+/* Stops the computer.
+ */
+ void halt(void) __attribute__((noreturn));
--- kernel/src/all/kernel/panic.c
+++ kernel/src/all/kernel/panic.c
@@ -1,7 +1,8 @@
+#include <kernel/panic.h>
 #include <kernel/tty.h>
-extern void halt(void);
+
 void panic(const char* msg) {
 	terminal_writestring("\n\nKernel panic: ");
 	terminal_writestring(msg);
-	halt(); //FIXME not declared
+	halt();
 }
